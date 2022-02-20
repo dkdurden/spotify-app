b39132e600e2847220f356c7fb4201f7b1296bc4
Dustin Durden <23153218+dkdurden@users.noreply.github.com>
2022-02-20 17:15:40 -0500

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,12 @@
+const redirect_uri =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3001"
+    : "https://determined-booth-c5c2f9.netlify.app";
+
 export const auth = {
   client_id: "5ba9957c47344e59adac7cb6d177cea9",
   response_type: "code",
-  redirect_uri: "http://localhost:3001",
+  redirect_uri,
   scope:
     "streaming user-read-email user-read-private user-library-read user-top-read user-library-modify user-read-playback-state user-modify-playback-state",
 };
