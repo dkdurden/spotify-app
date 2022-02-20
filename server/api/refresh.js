ef397bea5fc76d2b042ed9edeca6f10e3b1b522f
Dustin Durden <23153218+dkdurden@users.noreply.github.com>
2022-02-20 15:55:28 -0500

    Working on Vercel

---
+++ server/api/refresh.js
@@ -0,0 +1,29 @@
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const lyricsFinder = require("lyrics-finder");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+module.exports = (req, res) => {
+  const refreshToken = req.body.refreshToken;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+    });
+};
